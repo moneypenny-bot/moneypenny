36bfbfc09714d46914069e47a0cafe56a5daf848
John Wulff <johnw@orcasnet.com>
2011-09-04 13:12:17 -0700

    Update version responder to use VERSION constant set at load instead of version derived on the fly.

--- lib/moneypenny/responders/version.rb
+++ lib/moneypenny/responders/version.rb
@@ -6,15 +6,8 @@ module Moneypenny
 
     def self.respond(message)
       if (query = message.match(/\Aversion\z/i))
-        root = File.join File.dirname(__FILE__), '..', '..', '..'
-        if (git_sha = `cd #{root} && git rev-parse HEAD`.strip) == ''
-          version = File.read(File.join(root, 'VERSION')).strip
-          url     = "https://github.com/moneypenny-bot/moneypenny/tree/v#{version}"
-        else
-          version = git_sha[0..19]
-          url     = "https://github.com/moneypenny-bot/moneypenny/tree/#{git_sha}"
-        end
-        "Current Moneypenny version: #{version} (#{url})"
+        url = "https://github.com/moneypenny-bot/moneypenny/tree/#{VERSION}"
+        "Current Moneypenny version: #{VERSION} (#{url})"
       else
         false
       end
