951d43e32053b2fbcc1977a519d34f8831ac83c8
Doug Cole <doug@estately.com>
2011-09-04 23:06:19 -0700

    add responder plugin loader and remover

--- lib/moneypenny/responder.rb
+++ lib/moneypenny/responder.rb
@@ -1,5 +1,6 @@
 module Moneypenny
   class Responder
+    attr_accessor :loaded_plugins
     @registered_responders = []
 
     def self.inherited(subclass)
@@ -8,6 +9,30 @@ module Moneypenny
 
     def self.all
       @registered_responders
+    end
+
+    def initialize(options = {})
+      self.loaded_plugins = Set.new
+    end
+
+    def load_plugin!(plugin_name)
+      self.loaded_plugins << find_plugin_by_name(plugin_name)
+    end
+
+    def load_all!
+      self.class.all.each do |p|
+        load_plugin!(p.name)
+      end
+    end
+
+    def find_plugin_by_name(plugin_name)
+      plugin = self.class.all.find {|p| p.name == plugin_name}
+      raise ArgumentError, "Unknown plugin: #{plugin_name}" if plugin.nil?
+      plugin
+    end
+
+    def remove_plugin!(plugin_name)
+      self.loaded_plugins.delete find_plugin_by_name(plugin_name)
     end
   end
 end
