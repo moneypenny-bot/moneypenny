2a36aef887018b317026c68f8b853b4b78002cef
Doug Cole <doug@estately.com>
2011-10-25 01:05:07 -0700

    add authentication to issues responder

--- spec/responders/issues_spec.rb
+++ spec/responders/issues_spec.rb
@@ -17,14 +17,6 @@ describe Moneypenny::Plugins::Responders::Issues do
   end
 
   describe 'respond' do
-    repos = {
-      :user                => "brentvatne",
-      :repo_with_issues    => "always-has-issues",
-      :repo_without_issues => "never-has-issues",
-      :non_existent_repo   => "this-repo-does-not-exist",
-    }
-
-    successful_issues_matcher = /issue(s)? found.*? issue title text/mi
 
     context 'given a message that it understands' do
       it "should respond to 'show (me) issues'" do
@@ -35,7 +27,7 @@ describe Moneypenny::Plugins::Responders::Issues do
       end
 
       it "should respond to 'show (me) <user>/<repo> issues'" do
-        repo_path = "#{repos[:user]}/#{repos[:repo_with_issues]}"
+        repo_path = "brentvatne/always-has-issues"
         VCR.use_cassette('github-issues_repo_with_issues') do
           @issues.respond("show #{repo_path} issues").should match /\w/
           @issues.respond("show me #{repo_path} issues").should match /\w/
@@ -43,23 +35,39 @@ describe Moneypenny::Plugins::Responders::Issues do
       end
 
       it "should give information about the issues found when given a repository with open issues" do
-        repo_path = "#{repos[:user]}/#{repos[:repo_with_issues]}"
+        repo_path = "brentvatne/always-has-issues"
+        successful_issues_matcher = /issue(s)? found.*? issue title text/mi
         VCR.use_cassette('github-issues_repo_with_issues') do
           @issues.respond("show #{repo_path} issues").should match successful_issues_matcher
         end
       end
 
       it "must say if a repository does not exist" do
-        repo_path = "#{repos[:user]}/#{repos[:non_existent_repo]}"
+        repo_path = "brentvatne/this-repo-does-not-exist"
         VCR.use_cassette('github-issues_non_existent_repo') do
           @issues.respond("show #{repo_path} issues").should match /does not exist/
         end
       end
 
       it "must say if a repository has no open issues" do
-        repo_path = "#{repos[:user]}/#{repos[:repo_without_issues]}"
+        repo_path = "brentvatne/never-has-issues"
         VCR.use_cassette('github-issues_repo_without_issues') do
           @issues.respond("show #{repo_path} issues").should match /unable to find/
+        end
+      end
+
+      it "says if it is unable to authenticate for a repository" do
+        repo_path = "unauthenticated/unauthenticated"
+        VCR.use_cassette('github-issues_unauthenticated') do
+          @issues.respond("show #{repo_path} issues").should match /is private/
+        end
+      end
+
+      it "works with private repos given a login and token" do
+        repo_path = "authenticated/authenticated"
+        @issues.stubs(:config => { 'token' => 'TOKEN_STUB', 'login' => 'bar'} )
+        VCR.use_cassette('github-issues_authenticated') do
+          @issues.respond("show #{repo_path} issues").should match /2 issues found/
         end
       end
     end
